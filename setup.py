9ce5e5892b6ed8804e0597bf128a669f1b455bd0
matthieuEv <evainmatthieu44@gmail.com>
2023-07-13 16:36:41 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open('README.md', 'r') as f:
 
 setup(
     name='mail-Generator',
-    version='0.2.1',
+    version='0.2.2',
     packages=find_packages(),
     install_requires=[
         'mailslurp-client',
