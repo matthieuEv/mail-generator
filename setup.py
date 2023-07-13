4a85a3eea3f41496cad940e66b5ff9908aa86ad8
matthieuEv <evainmatthieu44@gmail.com>
2023-07-13 16:36:20 +0200

    update setup.py

--- setup.py
+++ setup.py
@@ -16,5 +16,7 @@ setup(
         ]
     },
     long_description=long_description,
-    long_description_content_type='text/markdown'
+    long_description_content_type='text/markdown',
+    url='https://github.com/matthieuEv/mail-generator',
+    description='A Python package that provides an easy-to-use interface for creating and managing email addresses'
 )
